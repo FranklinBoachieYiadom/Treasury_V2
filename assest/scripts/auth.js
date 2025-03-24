82aa8da960ff29d08c173dac13864ddc73a1dfb4
Phlash <a12@Phlashs-MacBook-Pro.local>
2025-03-24 16:46:07 +0500

    Added user roles, admin and member, and restrictions

--- assest/scripts/auth.js
+++ assest/scripts/auth.js
@@ -1,21 +1,27 @@
 //Checking if the user is logged in across all pages
 
 document.addEventListener('DOMContentLoaded', () => {
-    const checkAuth = () => {
+  const checkAuth = () => { 
       if (!sessionStorage.getItem("user-creds")) {
         window.location.href = "./index.html";
       }
     };
-  
-    checkAuth();
 
-    
+ checkAuth();
+
 let UserInfo=JSON.parse(sessionStorage.getItem("user-info"));
 const GreetHead=document.getElementById('msg-body');
+const Role =document.getElementById("admin-section")
+
+// Only show the menu nav link if the user role is admin
+if (Role){
+  if(UserInfo.role === "admin"){
+    Role.style.display ="block" ;
+  }
+}
+
 //This is for the landing page to get the Name of the user
 if (GreetHead){
     GreetHead.innerText = `Welcome ${UserInfo.fname}!`;
   }    
-
-
-  });
+});
