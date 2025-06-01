7f1710cbe4e68bc686b49ac452d76b378ec6d5fd
Franklin Boachie-Yiadom <franklinboachie@gmail.com>
2025-06-02 00:51:16 +0500

    Changed the conference webURL

--- church1/assest/webAppUrls/weburls.js
+++ church1/assest/webAppUrls/weburls.js
@@ -1,7 +1,7 @@
 
 const IncomeWebAppUrl = 'https://script.google.com/macros/s/AKfycbz6aKEzFGa8ztzvunW_9gYJj3AMsHZkHwyslb8aQzwWKU9_D08q04E85VrB8-0nemuWdQ/exec';
 const ExpenditureWebAppUrl = 'https://script.google.com/macros/s/AKfycbzsQuRy8MyJHE6qLJS3iS9pl1o0KikXz2DGKA7yGXgOuLdVv8eb3Z_brzvVNcl5-1a3IQ/exec';
-const ConferenceWebAppUrl = 'https://script.google.com/macros/s/AKfycbxQKdfHK1SvTKJDDoJK-niYQzVYvKv9zilBclHMvR-M81MOF3e12oYoHiPIV_QaC7TkxQ/exec';
+const ConferenceWebAppUrl = 'https://script.google.com/macros/s/AKfycbxytTEWen706OmR5fKey2qNIlRe_QwbTLVK_-DJPlHLsF7_REMV1rERiHjNnzB1OFeuvA/exec';
 const DistrictWebAppUrl = 'https://script.google.com/macros/s/AKfycbz7UCFcas-OBahG81Gutd1SkzmFncvLgX30m_y43ZAE7uzpwjBANbq_4SZAiU_aWO3R5A/exec';
 const LocalIncomeWebAppUrl = 'https://script.google.com/macros/s/AKfycbwJtMQTFn6sIT2z06yDEBzo39NLYURzkeECRKy6rep9WaTKZOWSCS7Y38bxCa63TBZDnw/exec';
 
